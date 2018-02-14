f71871a78751c8354d51dc39d0c5897cba6ef277
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-02-14 21:00:44 +0530

    Added activity handlers & seperated Dialog Concerns for the Bot

--- Math Dialog Bot/dialogs-basic/Controllers/MessagesController.cs
+++ Math Dialog Bot/dialogs-basic/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
+﻿using dialogs_basic.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System;
 using System.Net.Http;
@@ -20,90 +21,38 @@ namespace dialogs_basic
             {
                 await Conversation.SendAsync(activity, () => new MathsDialog());
             }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
 
             return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
-    }
 
-    [Serializable]
-    public class MathsDialog : IDialog<object>
-    {
-        // Bot Framework manages automatically persists per conversation data
-        protected int number1 { get; set; }
-        public async Task StartAsync(IDialogContext context)
+        private Activity HandleSystemMessage(Activity message)
         {
-            
-            context.Wait(MessageReceivedStart);
-        }
-        public async Task MessageReceivedStart(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            await context.PostAsync("do you want to add, square root, or squared?");
-
-            context.Wait(MessageReceivedOperationChoice);
-        }
-
-        public async Task MessageReceivedOperationChoice(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var message = await argument;
-
-            if (message.Text.ToLower().Equals("add", StringComparison.InvariantCultureIgnoreCase))
+            if (message.Type == ActivityTypes.DeleteUserData)
             {
-                await context.PostAsync("Provide number one:");
-                context.Wait(MessageReceivedAddNumber1);
+                
             }
-            else if (message.Text.ToLower().Equals("square root", StringComparison.InvariantCultureIgnoreCase))
+            else if (message.Type == ActivityTypes.ConversationUpdate)
             {
-                await context.PostAsync("Provide one number:");
-                context.Wait(MessageReceivedSquareRoot);
+                
             }
-            else if (message.Text.ToLower().Equals("squared", StringComparison.InvariantCultureIgnoreCase))
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
-                await context.PostAsync("Provide one number:");
-                context.Wait(MessageReceivedSquared);
+                
             }
-            else
+            else if (message.Type == ActivityTypes.Typing)
             {
-                context.Wait(MessageReceivedStart);
+                
             }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+
+            }
+
+            return null;
         }
-
-        public async Task MessageReceivedAddNumber1(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var numbers = await argument;
-            // number one is persisted between messages automatically by bot framework dialog
-            this.number1 = int.Parse(numbers.Text); 
-            await context.PostAsync("Provide number two:");
-
-            context.Wait(MessageReceivedAddNumber2);
-        }
-
-        public async Task MessageReceivedAddNumber2(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var numbers = await argument;
-            var number2 = int.Parse(numbers.Text);
-            await context.PostAsync($"{this.number1} + {number2} is = {this.number1+number2}");
-
-            context.Wait(MessageReceivedStart);
-        }
-
-        public async Task MessageReceivedSquareRoot(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var number = await argument;
-            var num = double.Parse(number.Text);
-
-            await context.PostAsync($"square root of {num} is {Math.Sqrt(num)}");
-
-            context.Wait(MessageReceivedStart);
-        }      
-        
-        public async Task MessageReceivedSquared(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var activity = await argument;
-            var num = double.Parse(activity.Text);
-
-            await context.PostAsync($"{num} squared is {num * num}");
-
-            context.Wait(MessageReceivedStart);
-        }  
     }
 }
